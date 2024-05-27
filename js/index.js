e5ae5a34b4e4cca603969226a82ab5f9ce4ba312
marigv95 <mama_guima04@hotmail.com>
2024-05-27 11:19:34 -0300

    Create Exibir

--- js/index.js
+++ js/index.js
@@ -1,34 +1,43 @@
-let tabuleiro
-let board
-let aviso
-let jogador
-let linha
-let coluna
+let tabuleiro;
+let board;
+let aviso;
+let jogador;
+let linha;
+let coluna;
 
-function iniciar(){
-    tabuleiro = []
-    board = document.getElementById('board')
-    aviso = document.getElementById('aviso')
-    jogador = 1
+function iniciar() {
+  tabuleiro = [];
+  board = document.getElementById("board");
+  aviso = document.getElementById("aviso");
+  jogador = 1;
 
-    for(let i = 0; i < 3; i ++){
-        tabuleiro[i] = []
-        for(let j = 0; j < 3; j++){
-            tabuleiro[i][j] = 0
-        }
+  for (let i = 0; i < 3; i++) {
+    tabuleiro[i] = [];
+    for (let j = 0; j < 3; j++) {
+      tabuleiro[i][j] = 0;
+    }
+  }
+
+  console.table(tabuleiro);
+  exibir();
+}
+
+function exibir() {
+  let tabela = '<table cellpadding ="10" border="1">';
+
+  for (let i = 0; i < 3; i++) {
+    tabela += "<tr>";
+
+    for (let j = 0; j < 3; j++) {
+      tabela += "<td>_</td>";
     }
 
-    console.table(tabuleiro)
+    tabela += "</tr>";
+  }
+  tabela += "</tabela>";
+  board.innerHTML = tabela;
 }
 
-function exibir(){
-    
-}
+function jogar() {}
 
-function jogar(){
-
-}
-
-function checar(){
-
-}
+function checar() {}
