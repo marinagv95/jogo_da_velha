c9b36faa9b18957cbbf3bfdff5cbef604d10609e
marigv95 <mama_guima04@hotmail.com>
2024-05-27 11:05:37 -0300

    Create board

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let tabuleiro
+let board
+let aviso
+let jogador
+let linha
+let coluna
+
+function iniciar(){
+    tabuleiro = []
+    board = document.getElementById('board')
+    aviso = document.getElementById('aviso')
+    jogador = 1
+
+    for(let i = 0; i < 3; i ++){
+        tabuleiro[i] = []
+        for(let j = 0; j < 3; j++){
+            tabuleiro[i][j] = 0
+        }
+    }
+
+    console.table(tabuleiro)
+}
+
+function exibir(){
+    
+}
+
+function jogar(){
+
+}
+
+function checar(){
+
+}
